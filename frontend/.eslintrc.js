fdedc2c1703605ea4a69f098c159c2a42371cfbc
Joona <joona.sandbacka@hotmail.fi>
2025-11-26 12:26:36 +0200

    Added pipleine, Update packages

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,13 +1,21 @@
 module.exports = {
   env: {
     browser: true,
+    node: true,
     es2021: true,
   },
   extends: [
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:react/jsx-runtime",
+    "react-app",
+    "react-app/jest",
   ],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
   overrides: [
     {
       env: {
