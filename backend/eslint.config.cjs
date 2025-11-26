f51289c0a3f98cca1cc964227db58088200d3b94
Joona <joona.sandbacka@hotmail.fi>
2025-11-26 12:44:20 +0200

    Update eslint setup

---
+++ backend/eslint.config.cjs
@@ -0,0 +1,23 @@
+const js = require("@eslint/js");
+const globals = require("globals");
+
+module.exports = [
+  js.configs.recommended,
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.node,
+      },
+    },
+    rules: {
+      indent: ["error", 2],
+      "linebreak-style": ["error", "unix"],
+      quotes: ["error", "double"],
+      semi: ["error", "always"],
+      "no-console": "off",
+    },
+  },
+];
